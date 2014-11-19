e53110fbd069dcd2c1fae2686503bf40ca85239d
Robert Truong <rtruong2@gmu.edu>
2014-11-19 12:03:48 -0500

    ThermDriver modified to include object declarations in main.

--- Thermostat/src/ThermDriver.java
+++ Thermostat/src/ThermDriver.java
@@ -1,18 +1,15 @@
-
 public class ThermDriver {
 
-	private PriHeat myHeater = new PriHeat(1);
-	private SecHeat myBackupHeater = new SecHeat(2);
-	private HVACDevice myAC = new HVACDevice(3);
-	private Fan myFan = new Fan(4);
-	private Temperature desiredTemp = new Temperature ();
-	private Temperature currentTemp = new Temperature ();
-	private TempControl myTempControl = new TempControl (currentTemp, desiredTemp, myAC, myHeater, myBackupHeater, myFan);
-	private Thermostat MyThermostat = new Thermostat(desiredTemp);
-	private IndoorTempSensor myIndoorTemp = new IndoorTempSensor (currentTemp);
-	
 	public static void main(String [] args)
 	{
-			//drives the system	; vroom vroom
+		PriHeat myHeater = new PriHeat(1);
+		SecHeat myBackupHeater = new SecHeat(2);
+		HVACDevice myAC = new HVACDevice(3);
+		Fan myFan = new Fan(4);
+		Temperature desiredTemp = new Temperature ();
+		Temperature currentTemp = new Temperature ();
+		TempControl myTempControl = new TempControl (currentTemp, desiredTemp, myAC, myHeater, myBackupHeater, myFan);
+		Thermostat myThermostat = new Thermostat(desiredTemp);
+		IndoorTempSensor myIndoorTemp = new IndoorTempSensor (currentTemp);
 	}
 }
