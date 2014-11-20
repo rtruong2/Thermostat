787b49fcd0d9a7e98e08821a359271590bfcb3c3
Robert Truong <rtruong2@gmu.edu>
2014-11-19 20:38:29 -0500

    Changes and stuff.
    Signed-off-by: Robert Truong <rtruong2@gmu.edu>

--- Thermostat/src/IndoorTempSensor.java
+++ Thermostat/src/IndoorTempSensor.java
@@ -1,9 +1,9 @@
+import java.util.*;
 
 public class IndoorTempSensor {
 	private long rawSensorValue;
-	private Temperature currentTemp = new Temperature ((float) rawSensorValue);
-	
-		
+	private Temperature currentTemp = new Temperature ((float) rawSensorValue);	
+
 	public IndoorTempSensor(Temperature currentTemp) 
 	{
 		this.rawSensorValue = 0;
@@ -12,9 +12,41 @@ public class IndoorTempSensor {
 
 	public void monitorTemp()
 	{
-		//do later
+		Scanner scan = new Scanner (System.in);
+
+		boolean breaker = false;
+		while(!breaker)
+		{
+			System.out.println("Press 1 to make the temperature go up .1 degree."
+					+ "\nPress 2 to make the temperature go down .1 degree."
+					+ "\nPress 3 to set the Temperature."
+					+ "\nPress 4 to exit out of monitorTemp.");
+
+			switch(scan.nextInt())
+			{
+			case 1:
+				currentTemp.set(currentTemp.get() + (float).1);
+				break;
+			case 2:
+				currentTemp.set(currentTemp.get() - (float).1);
+				break;
+			case 3:
+				System.out.println("Input temp.");
+				currentTemp.set(scan.nextFloat());
+				break;
+			case 4:
+				breaker = true;
+				break;
+			default:
+				break;
+			}
+
+			System.out.format("\ncurrentTemp is %f.", this.getCurrentTemp());
+		}
+		scan.close();
+		System.out.format("Temp is now %f.", this.getCurrentTemp());
 	}
-	
+
 	public float getCurrentTemp()
 	{
 		return currentTemp.get();
--- Thermostat/src/ThermDriver.java
+++ Thermostat/src/ThermDriver.java
@@ -1,4 +1,7 @@
-public class ThermDriver {
+import java.util.*;
+
+public class ThermDriver 
+{
 
 	public static void main(String [] args)
 	{
@@ -7,9 +10,66 @@ public class ThermDriver {
 		HVACDevice myAC = new HVACDevice(3);
 		Fan myFan = new Fan(4);
 		Temperature desiredTemp = new Temperature ();
-		Temperature currentTemp = new Temperature ();
+		Temperature currentTemp = new Temperature (70);
 		TempControl myTempControl = new TempControl (currentTemp, desiredTemp, myAC, myHeater, myBackupHeater, myFan);
 		Thermostat myThermostat = new Thermostat(desiredTemp);
 		IndoorTempSensor myIndoorTemp = new IndoorTempSensor (currentTemp);
+
+		Scanner scan = new Scanner(System.in);
+
+		while (true)
+		{
+			System.out.println("Enter option for Thermostat. "
+					+ "\n1. To turn it off or on."
+					+ "\n2. To set a desired temperature."
+					+ "\n3. To close the program.");
+			boolean breaker = false; //breaker is a flag that's intended to break out of the program's otherwise infinite loop of running.
+
+			switch(scan.nextInt())
+			{
+			case 1:
+				System.out.format("The system is currently on %s mode."
+						+ "\nPress 1 to turn it on."
+						+ "\nPress 2 to turn it off.\n", myTempControl.getCurrentMode());
+				switch(scan.nextInt())
+				{
+				case 1:
+					myTempControl.setMode(Mode.FANONLY);
+					System.out.println("The thermostat is on now.");
+					break;
+				case 2:
+					myTempControl.setMode(Mode.OFF);
+					System.out.println("The thermostat is off now.");
+					break;
+				}
+				break;
+			case 2:
+				System.out.println("Input desired temp.");
+				myThermostat.setDesiredTemp(scan.nextInt());
+			case 3:
+				breaker = true;
+				break;
+			default:
+				System.out.println("Not an option");
+				break;
+			}
+			if(breaker)
+				break;
+		}
+
+		scan.close();
+
+	}
+	
+	public void controlTemp(Temperature desiredTemp, Temperature currentTemp, TempControl myTempControl)
+	{
+		if (desiredTemp.get() - currentTemp.get() < -1.0)
+		{
+			myTempControl.setMode(Mode.HEAT);
+		}
+		else if (desiredTemp.get() - currentTemp.get() > 1.0)
+		{
+			
+		}
 	}
 }
